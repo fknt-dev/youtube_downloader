3d11ba4355b89f1a576c29e131711ba7e57c859f
fknt00 <fknt00@gmail.com>
2023-02-12 21:55:04 -0300

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -3,37 +3,39 @@ import subprocess
 from pytube import YouTube
 from os import getcwd
 
-def mixar_arquivos(nome_do_arquivo):
+def mixar_arquivos(nome_do_video):
     pasta_local = getcwd() + '\\Videos\\'
+    pasta_temporaria = getcwd() + '\\Temp\\'
 
-    video = pasta_local + nome_do_arquivo + '.vp9'
-    audio = pasta_local + nome_do_arquivo + '.opus'
-    saida = pasta_local + nome_do_arquivo + '.mkv'
+    video = pasta_temporaria + nome_do_video + '-video.webm'
+    audio = pasta_temporaria + nome_do_video + '-audio.webm'
+    saida = pasta_local + nome_do_video + '.mkv'
 
     subprocess.run(f'mkvmerge -o "{saida}" "{video}" "{audio}" -q')
-    os.remove(f'{video}'); os.remove(f'{audio}')
+    os.remove(f'{video}'); os.remove(f'{audio}'); os.removedirs(pasta_temporaria)
 
 def main():
-    url_do_video = input('Digite o link: ')
+    url_do_video = input('Digite o link do vídeo: ')
     yt = YouTube(url_do_video)
 
-    pasta_local = getcwd() + '\\Videos\\'
-    print(pasta_local)
-
-    nome_do_arquivo = yt.title
-    print(f'Baixando: "{nome_do_arquivo}"')
+    pasta_destino = getcwd() + '\\Videos\\'
+    pasta_temporaria = getcwd() + '\\Temp\\'
 
     video = yt.streams.get_by_itag(248)
     audio = yt.streams.get_by_itag(251)
 
-    tamanho_em_megabytes = round(video.filesize_mb, 2)
+    titulo_do_video = yt.title
+    print(f'Baixando: "{titulo_do_video}"')
 
-    print(f'Tamanho: {tamanho_em_megabytes}mb')
+    video.download(pasta_temporaria, titulo_do_video+'-video.webm')
+    audio.download(pasta_temporaria, titulo_do_video+'-audio.webm')
 
-    video.download(output_path=pasta_local, filename=nome_do_arquivo+'.vp9')
-    audio.download(output_path=pasta_local, filename=nome_do_arquivo+'.opus')
+    localização = pasta_destino + titulo_do_video + '.mkv'
+    print(f'Localização: "{localização}"')
+    tamanho_em_mb = round(video.filesize_mb + audio.filesize_mb, 2)
+    print(f'Tamanho do arquivo: {tamanho_em_mb} MB')
 
-    mixar_arquivos(nome_do_arquivo)
+    mixar_arquivos(titulo_do_video)
 
 if __name__ == '__main__':
     main()
