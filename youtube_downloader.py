0b380891d6e753e884bf1f79765bf1eaa8650e58
fknt00 <fknt00@gmail.com>
2023-02-13 00:41:14 -0300

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,8 +1,9 @@
 from os import getcwd, remove, removedirs
 from pytube import YouTube
+from pytube.cli import on_progress
 from subprocess import run
 
-pasta_destino = getcwd() + '\\Vídeos\\'
+pasta_destino = getcwd() + '\\Vídeos baixados\\'
 pasta_temporaria = getcwd() + '\\Temp\\'
 
 def mixar_arquivos(titulo_do_video):
@@ -15,16 +16,16 @@ def mixar_arquivos(titulo_do_video):
 
 def gerenciador_de_downloads():
     url_do_video = input('Digite o link do vídeo: ')
-    yt = YouTube(url_do_video)
-    titulo_do_video = yt.title
+    objeto = YouTube(url_do_video, on_progress_callback=on_progress)
+    titulo_do_video = objeto.title
 
     caracteres_especiais = '\/:*?"<>|'
     for caractere in caracteres_especiais:
         if caractere in titulo_do_video:
             titulo_do_video = titulo_do_video.replace(caractere, '-')
 
-    video = yt.streams.get_by_itag(248)
-    audio = yt.streams.get_by_itag(251)
+    video = objeto.streams.filter(adaptive=True, file_extension='webm', type='video').first()
+    audio = objeto.streams.filter(adaptive=True, file_extension='webm', type='audio').last()
 
     print(f'Baixando: "{titulo_do_video}"')
     video.download(pasta_temporaria, f'{titulo_do_video}-video.webm')
@@ -34,7 +35,7 @@ def gerenciador_de_downloads():
 
     localização = pasta_destino + titulo_do_video + '.mkv'
     tamanho_em_mb = round(video.filesize_mb + audio.filesize_mb, 2)
-    print(f'Localização: "{localização}"')
+    print(f'O vídeo foi salvo em: "{localização}"')
     print(f'Tamanho do arquivo: {tamanho_em_mb} MB')
 
 if __name__ == '__main__':
